dd152905f0078663c932ee31b1d624c67919415f
codergeek69 <60755811+codergeek69@users.noreply.github.com>
2022-03-02 14:23:23 +0200

    Add files via upload

---
+++ Unity 3D/Assets/DistanceComReader.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+public class DistanceComReader : MonoBehaviour {
+    public ObstacalWorld TheObsticalWorld;
+    SerialPort sp;
+  public  string comPort = "COM7";
+	// Use this for initialization
+	void Start () {
+        TheObsticalWorld = new ObstacalWorld();
+        sp = new SerialPort();
+        sp.DataReceived += serialPort1_DataReceived;
+        sp.PortName = comPort;
+        sp.BaudRate = 9600;
+        sp.DtrEnable = true;
+        sp.Open();
+        if (sp.IsOpen)
+        {
+            print("Open");
+        }
+	}
+    private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+    {
+        print("Start Read"); 
+        string line = sp.ReadLine();
+        print(line);
+      //  this.BeginInvoke(new LineReceivedEvent(LineReceived), line);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        string line = sp.ReadLine();
+        print(line);
+        TheObsticalWorld.AddLine(line);
+	}
+    public class ObstacalWorld
+    {
+        public Dictionary<int, Obstical> AllObstacles= new Dictionary<int,Obstical>();
+        public void AddLine(string data)
+        {
+            if (data.Length > 0)
+            {
+                string[] split = data.Split(new string[] { " " }, System.StringSplitOptions.RemoveEmptyEntries);
+                int angle = int.Parse(split[0]);
+                int dist0 = int.Parse(split[1]);
+                int dist1 = int.Parse(split[2]);
+                int dist2 = int.Parse(split[3]);
+
+                if (AllObstacles.ContainsKey(angle))
+                {
+                    Obstical obs = AllObstacles[angle];
+                    obs.Distance = dist0;
+                }
+                else
+                {
+                    Obstical obs = new Obstical();
+                    obs.Angle = angle;
+                    obs.Distance = dist0;
+                    AllObstacles.Add(angle, obs);
+            
+                }
+            }
+
+        }
+    }
+    public class Obstical
+    {
+        public int Angle;
+        public int Distance;
+        public List<int> LastDistances = new List<int>();
+
+    }
+}
---
+++ Unity 3D/Assets/ObsticalDrawer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsticalDrawer : MonoBehaviour
+{
+    public GameObject PrefabObstacle;
+    public DistanceComReader TheReader;
+    int currentIndex = 0;
+    Dictionary<int, GameObject> Spawns = new Dictionary<int, GameObject>();
+
+    public GameObject LiveCube;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 angle = transform.eulerAngles;
+        angle.y = -currentIndex;
+        transform.eulerAngles = angle;
+        if (currentIndex > TheReader.TheObsticalWorld.AllObstacles.Count)
+        {
+            currentIndex = 0;
+        }
+        else
+        {
+            DistanceComReader.Obstical anObs = null;
+            if (TheReader.TheObsticalWorld.AllObstacles.ContainsKey(currentIndex))
+            {
+                anObs = TheReader.TheObsticalWorld.AllObstacles[currentIndex];
+                Vector3 loc = LiveCube.gameObject.transform.localPosition;
+                loc.z = anObs.Distance;
+                LiveCube.gameObject.transform.localPosition = loc;
+                if (Spawns.ContainsKey(currentIndex))
+                {
+                    GameObject anObj = Spawns[currentIndex];
+
+                    Vector3 loc2 = LiveCube.gameObject.transform.position;// LiveCube.gameObject.transform.localPosition;
+                    //loc.z = anObs.Distance;
+                    anObj.transform.position = loc2;
+                }
+                else
+                {
+                    GameObject anObj = SpawnNew(anObs.Angle);
+                    Spawns.Add(anObs.Angle, anObj);
+                    Vector3 loc2 = LiveCube.gameObject.transform.position;// LiveCube.gameObject.transform.localPosition;
+                    //loc.z = anObs.Distance;
+                    anObj.transform.position = loc2;
+                }
+            }
+        }
+        currentIndex++;
+    }
+    private GameObject SpawnNew(int angle)
+    {
+        GameObject anObj = GameObject.Instantiate(PrefabObstacle);
+        anObj.name = "A" + angle;
+        return anObj;
+    }
+}
